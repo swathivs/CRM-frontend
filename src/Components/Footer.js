6d81cf8b0528426fef1694bdd007c772f701b622
swathivs <swathivs1406@gmail.com>
2023-09-14 17:52:14 +0530

    commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer bg-light pb-5">
+      <div className="external-links copyrights">
+        <div>Copyrights © 2023| All Rights Reserved</div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
